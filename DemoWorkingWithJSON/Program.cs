e89ed59b80ff9d4761339bba7b49de90e6d4a890
DaoDucDev <daoducdev@gmail.com>
2020-10-12 19:03:24 +0700

    Demo working with JSON in C#

---
+++ DemoWorkingWithJSON/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace DemoWorkingWithJSON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cài NewtonSoft
+            //dotnet add package NewtonSoft.Json
+
+            List<Student> listStudents = new List<Student>();
+
+            listStudents.Add(new Student("Dao Van A", 21, "Thai Binh"));
+            listStudents.Add(new Student("Dao Van B", 21, "Ha Noi"));
+            listStudents.Add(new Student("Dao Van C", 21, "Hai Phong"));
+            listStudents.Add(new Student("Dao Van D", 21, "Thanh Hoa"));
+            listStudents.Add(new Student("Dao Van E", 21, "Nghe An"));
+
+            //Chuyển collection -> JSON Text
+            string jsonText = JsonConvert.SerializeObject(listStudents);
+
+            Console.WriteLine(jsonText);
+            
+            //Chuyển text json -> collection
+            List<Student> listFromJsonText = JsonConvert.DeserializeObject<List<Student>>(jsonText);
+        }
+    }
+}
---
+++ DemoWorkingWithJSON/Student.cs
@@ -0,0 +1,32 @@
+class Student
+{
+    private string name;
+    public string Name
+    {
+        get { return name; }
+        set { name = value; }
+    }
+
+    private int age;
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    private string address;
+    public string Address
+    {
+        get { return address; }
+        set { address = value; }
+    }
+
+    public Student() { }
+    public Student(string _name, int _age, string _address)
+    {
+        Name = _name;
+        Age = _age;
+        Address = _address;
+    }
+
+}
